bebdc5ccf201aead6db4d0efe28af59e1b233f36
kyeoeol <kyeoeol@gmail.com>
2022-07-04 22:30:10 +0900

    Drawing Paths and Shapes init

---
+++ DrawingandAnimation/DrawingandAnimation/BadgeBackground.swift
@@ -0,0 +1,28 @@
+//
+//  BadgeBackground.swift
+//  DrawingandAnimation
+//
+//  Created by kyeoeol on 2022/07/04.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(to: CGPoint(
+                x: width * 0.95,
+                y: height * 0.20
+            ))
+        }
+        .fill(.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
---
+++ DrawingandAnimation/DrawingandAnimation/DrawingandAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingandAnimationApp.swift
+//  DrawingandAnimation
+//
+//  Created by kyeoeol on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct DrawingandAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
